77c098d58e343c17dfd7ed2719ee50ba54cd08b3
Ian Mayton <ian.mayton@gmail.com>
2020-08-13 15:53:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,14 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade, :id 
+  
+  @@all = []
+  
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade
+    @id = id
+    @@all << self
+  end 
   
 end
